3a50daa67605e6ccbc94097d2fa5f6c02d3f90af
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-13 14:04:29 +0300

    print komanda

---
+++ 1_diena/print.py
@@ -0,0 +1,17 @@
+print("Sveika, pasaule!")
+print()
+print("Šodien ir saulaina diena")
+print()
+
+print("piens olas maize biezpiens")
+print("piens" , "olas" , "maize" , "biezpiens")
+print("piens" , "olas" , "maize" , "biezpiens" , sep="*#*")
+
+print("piens" , "olas" , "maize" , "biezpiens" , end="-beigas-")
+print("šī ir pēdējā rindiņa")
+
+print("piens" , "olas" , "maize" , "biezpiens" , sep="***" , end="-beigas-")
+print()
+
+print("šodien\ngribas\niet\nārā\npastaigāties\n")
+print("Šodien ir muzeju nakts 2023")
