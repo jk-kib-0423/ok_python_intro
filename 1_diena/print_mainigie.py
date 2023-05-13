46604a67ac0a01034c68c1c58c2e058533dcd49c
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-13 16:28:27 +0300

    drukasana

---
+++ 1_diena/print_mainigie.py
@@ -0,0 +1,12 @@
+vards = Oļesja
+uzvards = Karpoveca
+vertejums = 7
+augums = 1.8
+
+#vēlamais rezultāts Vārds: Oļesja, Uzvārds: Karpoveca, Vērtējums:7, Augums:1.8
+
+print("Vārds:" + vards + ", Uzvārds:" + uzvards + ", Vērtējums:" + str (vertejums) + ", Augums:" + str (augums)")
+
+print("Vārds:", vards, "Uzvārds:", uzvards, "Vērtējums:", vertejums, "Augums:", augums")
+
+print("Vārds: {}, Uzvārds: {}, Vērtējums: {}, Augums: {}".format(vards, uzvards, vertejums, augums))
