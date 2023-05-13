533bf00b6f5f852d1fa48f0bde79d1613daddde3
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-13 17:09:50 +0300

    matematika

---
+++ 1_diena/matematika.py
@@ -0,0 +1,33 @@
+import math
+
+sk1 = 5
+sk2 = 3
+
+print(f"sk1={sk1}, sk2={sk2}")
+
+print(sk1 + sk2)
+print(sk1, "+", sk2, "=", sk1 + sk2)
+
+rezultats = sk1 + sk2
+print("Rezultāts:", rezultats)
+
+print(f"{sk1} - {sk2} = {sk1 - sk2}")
+print(f"{sk1} / {sk2} = {sk1 / sk2}")
+
+print(f"{sk1} ** 2 = {sk1 ** 2}") #kāpināšana
+
+print("================================")
+sk1 = 5
+sk2 = 3
+cipari_aiz_komata = 4
+apalots = sk1 / sk2
+apalots = round(apalots, cipari_aiz_komata)
+print("Apaļosanas rezultāts:" , apalots)
+
+malaA = 5
+malaB = 4
+laukums = malaA * malaB
+print("Laukums:" , laukums)
+
+print(math.pi)
+
--- 1_diena/print_mainigie.py
+++ 1_diena/print_mainigie.py
@@ -5,7 +5,7 @@ augums = 1.8
 
 #vēlamais rezultāts Vārds: Oļesja, Uzvārds: Karpoveca, Vērtējums:7, Augums:1.8
 
-print("Vārds:" + vards + ", Uzvārds:" + uzvards + ", Vērtējums:" + str (vertejums) + ", Augums:" + str (augums)")
+print("Vārds:" + vards + ", Uzvārds:" + uzvards + ", Vērtējums:" + str(vertejums) + ", Augums:" + str(augums)")
 
 print("Vārds:", vards, "Uzvārds:", uzvards, "Vērtējums:", vertejums, "Augums:", augums")
 
