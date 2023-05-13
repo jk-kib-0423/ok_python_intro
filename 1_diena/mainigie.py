3d668e13775354fdadbbaac786ea7799ef7b6fdd
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-13 16:02:15 +0300

    stils

---
+++ 1_diena/mainigie.py
@@ -0,0 +1,46 @@
+#kastītes nosaukums - mainīgā nosaukums
+#kastītes veids - datu tips
+#kastītes saturs - mainīgā vērtība
+
+#vienādības zīme (=) ir piešķiršanas operators
+uzvards = "Karpoveca"
+
+print(uzvards)
+print("uzvards")
+
+uzvards = "Ozols"
+
+print(uzvards)
+
+print("===================================")
+
+print(type(uzvards)) #datu tips str - string jeb simbolu virkne
+teikums = "šī ir gara simbolu rinda"
+print(teikums)
+print(type(teikums))
+
+vertejums = 8
+print(vertejums)
+print(type(vertejums)) #daty tips int - integer jeb vesels skaitlis
+
+skaits = "5"
+print(skaits)
+print(type(skaits))
+
+attalums = 5.7
+print(attalums)
+print(type(attalums)) #datu tips - float (f) jeb racionāls skaitlis
+
+brilles = True
+print(brilles)
+print(type(brilles)) #datu tips - bool var saturēt vērtību True vai False
+
+tips = -
+Tips = gg #dažādie manīgie
+
+help("keywords") #python rezervētie vārdie, kurus nevar izmantot ka mainīgā nosaukums (reģistra jūtīgie)
+
+
+
+
+
---
+++ 1_diena/stils.py
@@ -0,0 +1,18 @@
+#Stila vadlīnijas https://peps.python.org/pep-0008/
+# - mainīgo nosaukumu jāveido tā, lai varētu saprast tā saturu, izvairīties no nosaukumiem x, y, z, a,
+#- mainīgo nosaukumā var būt cipars, bet mainīgā nosaukums nevar sākties ar ciparu
+vards2 = Peteris
+# 3vards = Mikelis # šis nav pareizi
+# - mainīgā nosaukums jāraksta ar maziem burtiem, ja tas satur vairākus vārdus, tad atdala ar komatu
+camelCaseStyle = "stils"
+snake_case_style = "stils"
+noncasestyle = "nestils" #nepareizi
+
+#visi lielie burti tiek lietoti, ja veidojam konstantus
+GRAVITACIJAS_PAATRINAJUMS = 9.8
+
+# - nekādā gadījumā neizmantojam garumzīmes un atstarpes
+# mans mainīgais = 5 # nepareizi
+# vārds = "Māris" nepareizi
+# print(vārds)
+
