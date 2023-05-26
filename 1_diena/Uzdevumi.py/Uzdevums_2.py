592f787abbb0a5a196fca02550a3fdf687410a9f
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-26 23:04:02 +0300

    labojums

--- 1_diena/Uzdevumi.py/Uzdevums_2.py
+++ 1_diena/Uzdevumi.py/Uzdevums_2.py
@@ -1,4 +1,4 @@
 print("Ko Saulīte gauži raud,")
-print("Ik vakaru noiedama?")
-print("Zelta josta iekritusi")
-print("Dziļas jūras dibenā.")
+print(" " "Ik vakaru noiedama?")
+print(" " " " "Zelta josta iekritusi")
+print(" " " " " " "Dziļas jūras dibenā.")
