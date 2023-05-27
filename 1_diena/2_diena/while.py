2c23ef9564f895bc2652785264929668c38df919
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-27 12:52:09 +0300

    lab

---
+++ 1_diena/2_diena/while.py
@@ -0,0 +1,22 @@
+
+
+sk1 = 5
+sk2 = 10
+
+while sk1 < sk2:
+    print("Izpildās cikla 1 rindiņa")
+    print("Izpildās cikla 2 rindiņa")
+    print("Izpildās cikla 3 rindiņa")
+    sk1 = sk1 + 1
+
+    #break
+    #continue
+
+start = 55
+stop = 75
+
+while True:
+    print("Šodien lielais hokejs")
+    if start >= stop:
+        break
+    start = start + 3
