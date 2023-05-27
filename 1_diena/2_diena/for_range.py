a52c28e76a84d36a130c57a6b683225484277b39
Olesja Karpovec <okarpoveca@gmail.com>
2023-05-27 11:54:47 +0300

    for_range

---
+++ 1_diena/2_diena/for_range.py
@@ -0,0 +1,25 @@
+for i in range(10):
+    print(i)
+
+    print("=======================")
+    for i in range(15, 50):
+     print(i, end=",")
+
+print("=======================")
+for i in range(66, 99, 3):
+     print(i, end=",")
+
+print("=======================")
+for i in range(80, 20, -2):
+    print(i, end=",")
+
+print("==============================")
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i * j, end="\t")
+
+    print()
+
+
+    
